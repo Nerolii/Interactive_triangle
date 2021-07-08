335ed7a08a59c0a83b729be42cb334fdc6ff9c32
Nerolii <wndud3323@naver.com>
2021-07-08 19:44:20 +0900

    First commit

---
+++ js/app.js
@@ -0,0 +1,23 @@
+class App {
+  constructor() {
+    this.canvas = document.createElement('canvas');
+    document.body.appendChild(this.canvas);
+    this.ctx = this.canvas.getContext('2d');
+
+    this.pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
+
+    window.addEventListener('resize', this.resize.bind(this), false);
+  }
+
+  resize() {
+    this.stageWidth = document.body.clientWidth;
+    this.stageHeight = document.body.clientHeight;
+
+    this.canvas.width = this.stageWidth * this.pixelRatio;
+    this.canvas.height = this.stageHeight * this.pixelRatio;
+  }
+}
+
+window.onload = () => {
+  new App();
+}
